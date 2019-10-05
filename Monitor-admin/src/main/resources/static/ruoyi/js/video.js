cb228061f0a5f6b87adb383fb231710599371221
MinManong <705047655@qq.com>
2019-10-06 01:17:33 +0800

    删除多余项目副本

--- Monitor-admin/src/main/resources/static/ruoyi/js/video.js
+++
@@ -1,9 +0,0 @@
-var src = "http://192.168.2.254:7086/live/cameraid/1000078$0/substream/1.m3u8";
-  var hls = new Hls();
-  var video = document.getElementById("videoCell");
-  video.src=src;
-  hls.loadSource(src);
-  hls.attachMedia(video);
-  hls.on(Hls.Events.MANIFEST_PARSED, function(){
-    video.play();
-  });
