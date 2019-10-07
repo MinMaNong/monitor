474b99d86cae3db6944ccf1541450efbb23b8319
goldenyukai <1073578300@qq.com>
2019-10-07 14:52:20 +0800

    第一次合并

---
+++ monitor/Monitor-admin/src/main/java/com/dejiacheng/web/controller/system/SysAlarmInfoController.java
@@ -0,0 +1,125 @@
+package com.dejiacheng.web.controller.system;
+
+import java.util.List;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.dejiacheng.common.annotation.Log;
+import com.dejiacheng.common.enums.BusinessType;
+import com.dejiacheng.system.domain.SysAlarmInfo;
+import com.dejiacheng.system.service.ISysAlarmInfoService;
+import com.dejiacheng.common.core.controller.BaseController;
+import com.dejiacheng.common.core.domain.AjaxResult;
+import com.dejiacheng.common.utils.poi.ExcelUtil;
+import com.dejiacheng.common.core.page.TableDataInfo;
+
+/**
+ * 系统报警信息Controller
+ * 
+ * @author yukai
+ * @date 2019-10-06
+ */
+@Controller
+@RequestMapping("/system/alarminfo")
+public class SysAlarmInfoController extends BaseController
+{
+    private String prefix = "system/alarminfo";
+
+    @Autowired
+    private ISysAlarmInfoService sysAlarmInfoService;
+
+    @RequiresPermissions("system:alarminfo:view")
+    @GetMapping()
+    public String alarminfo()
+    {
+        return prefix + "/alarminfo";
+    }
+
+    /**
+     * 查询系统报警信息列表
+     */
+    @RequiresPermissions("system:alarminfo:list")
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list(SysAlarmInfo sysAlarmInfo)
+    {
+        startPage();
+        List<SysAlarmInfo> list = sysAlarmInfoService.selectSysAlarmInfoList(sysAlarmInfo);
+        return getDataTable(list);
+    }
+
+    /**
+     * 导出系统报警信息列表
+     */
+    @RequiresPermissions("system:alarminfo:export")
+    @PostMapping("/export")
+    @ResponseBody
+    public AjaxResult export(SysAlarmInfo sysAlarmInfo)
+    {
+        List<SysAlarmInfo> list = sysAlarmInfoService.selectSysAlarmInfoList(sysAlarmInfo);
+        ExcelUtil<SysAlarmInfo> util = new ExcelUtil<SysAlarmInfo>(SysAlarmInfo.class);
+        return util.exportExcel(list, "alarminfo");
+    }
+
+    /**
+     * 新增系统报警信息
+     */
+    @GetMapping("/add")
+    public String add()
+    {
+        return prefix + "/add";
+    }
+
+    /**
+     * 新增保存系统报警信息
+     */
+    @RequiresPermissions("system:alarminfo:add")
+    @Log(title = "系统报警信息", businessType = BusinessType.INSERT)
+    @PostMapping("/add")
+    @ResponseBody
+    public AjaxResult addSave(SysAlarmInfo sysAlarmInfo)
+    {
+        return toAjax(sysAlarmInfoService.insertSysAlarmInfo(sysAlarmInfo));
+    }
+
+    /**
+     * 修改系统报警信息
+     */
+    @GetMapping("/edit/{alarmId}")
+    public String edit(@PathVariable("alarmId") Long alarmId, ModelMap mmap)
+    {
+        SysAlarmInfo sysAlarmInfo = sysAlarmInfoService.selectSysAlarmInfoById(alarmId);
+        mmap.put("sysAlarmInfo", sysAlarmInfo);
+        return prefix + "/edit";
+    }
+
+    /**
+     * 修改保存系统报警信息
+     */
+    @RequiresPermissions("system:alarminfo:edit")
+    @Log(title = "系统报警信息", businessType = BusinessType.UPDATE)
+    @PostMapping("/edit")
+    @ResponseBody
+    public AjaxResult editSave(SysAlarmInfo sysAlarmInfo)
+    {
+        return toAjax(sysAlarmInfoService.updateSysAlarmInfo(sysAlarmInfo));
+    }
+
+    /**
+     * 删除系统报警信息
+     */
+    @RequiresPermissions("system:alarminfo:remove")
+    @Log(title = "系统报警信息", businessType = BusinessType.DELETE)
+    @PostMapping( "/remove")
+    @ResponseBody
+    public AjaxResult remove(String ids)
+    {
+        return toAjax(sysAlarmInfoService.deleteSysAlarmInfoByIds(ids));
+    }
+}
---
+++ monitor/Monitor-system/src/main/java/com/dejiacheng/system/domain/SysAlarmInfo.java
@@ -0,0 +1,122 @@
+package com.dejiacheng.system.domain;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import com.dejiacheng.common.annotation.Excel;
+import com.dejiacheng.common.core.domain.BaseEntity;
+import java.util.Date;
+
+/**
+ * 系统报警信息对象 sys_alarm_info
+ * 
+ * @author yukai
+ * @date 2019-10-06
+ */
+public class SysAlarmInfo extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
+
+    /** 报警id，主键 */
+    private Long alarmId;
+
+    /** 下发人员 */
+    @Excel(name = "下发人员")
+    private String alarmPerson;
+
+    /** 时间 */
+    @Excel(name = "时间", width = 30, dateFormat = "yyyy-MM-dd")
+    private Date alarmTime;
+
+    /** 位置 */
+    @Excel(name = "位置")
+    private String alarmPlace;
+
+    /** 类型 */
+    @Excel(name = "类型")
+    private String alarmType;
+
+    /** 处理人员 */
+    @Excel(name = "处理人员")
+    private String solvePerson;
+
+    /** 处理状态 */
+    @Excel(name = "处理状态")
+    private String solveStatus;
+
+    public void setAlarmId(Long alarmId) 
+    {
+        this.alarmId = alarmId;
+    }
+
+    public Long getAlarmId() 
+    {
+        return alarmId;
+    }
+    public void setAlarmPerson(String alarmPerson) 
+    {
+        this.alarmPerson = alarmPerson;
+    }
+
+    public String getAlarmPerson() 
+    {
+        return alarmPerson;
+    }
+    public void setAlarmTime(Date alarmTime) 
+    {
+        this.alarmTime = alarmTime;
+    }
+
+    public Date getAlarmTime() 
+    {
+        return alarmTime;
+    }
+    public void setAlarmPlace(String alarmPlace) 
+    {
+        this.alarmPlace = alarmPlace;
+    }
+
+    public String getAlarmPlace() 
+    {
+        return alarmPlace;
+    }
+    public void setAlarmType(String alarmType) 
+    {
+        this.alarmType = alarmType;
+    }
+
+    public String getAlarmType() 
+    {
+        return alarmType;
+    }
+    public void setSolvePerson(String solvePerson) 
+    {
+        this.solvePerson = solvePerson;
+    }
+
+    public String getSolvePerson() 
+    {
+        return solvePerson;
+    }
+    public void setSolveStatus(String solveStatus) 
+    {
+        this.solveStatus = solveStatus;
+    }
+
+    public String getSolveStatus() 
+    {
+        return solveStatus;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+            .append("alarmId", getAlarmId())
+            .append("alarmPerson", getAlarmPerson())
+            .append("alarmTime", getAlarmTime())
+            .append("alarmPlace", getAlarmPlace())
+            .append("alarmType", getAlarmType())
+            .append("solvePerson", getSolvePerson())
+            .append("solveStatus", getSolveStatus())
+            .toString();
+    }
+}
